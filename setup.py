d0465335795b54d191afafebd3e598c2f6c4b0e3
Bill Mania <bill@manialabs.us>
2024-12-30 16:34:17 -0600

    Create an autonomy_demo.launch.py and autonomy_demo.sh, which start an RPLiDAR node and an apriltag node.
    https://github.com/billmania/roboquest_addons/issues/7

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ package_name = 'roboquest_addons'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.4.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.yml')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
