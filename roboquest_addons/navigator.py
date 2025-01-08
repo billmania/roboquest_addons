e4118488c239bdc6a5d0e1a8f00951d32706857c
Bill Mania <bill@manialabs.us>
2025-01-08 17:04:51 -0600

    Consumes APRIL tag detections and publishes appropriate Twist messages.
    https://github.com/billmania/roboquest_addons/issues/7

--- roboquest_addons/navigator.py
+++ roboquest_addons/navigator.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python3
+
 """Follow a path using APRIL tags as the waypoints."""
 from apriltag_msgs.msg import AprilTagDetectionArray
+
+from geometry_msgs.msg import TwistStamped
 
 import rclpy
 import rclpy.logging
 from rclpy.node import Node
 
+from rq_msgs.srv import Control
+
 from tf2_msgs.msg import TFMessage
+
+CLOSE_ENOUGH = 10
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
 
 
 class Navigation(Node):
@@ -24,6 +34,15 @@ class Navigation(Node):
         """
         super().__init__('navigation')
         self._log = self.get_logger()
+
+        self._twist = TwistStamped()
+        self._control_request = Control.Request()
+
+        self._camera = {
+            'x_center': int(CAMERA_WIDTH / 2),
+            'y_center': int(CAMERA_HEIGHT / 2)
+        }
+
         self._detection_sub = self.create_subscription(
             AprilTagDetectionArray,
             'detections',
@@ -36,23 +55,84 @@ class Navigation(Node):
             self._tf_cb,
             1
         )
+        self._cmd_vel_pub = self.create_publisher(
+            TwistStamped,
+            'cmd_vel',
+            1
+        )
+        self._control_client = self.create_client(
+            Control,
+            'control_hat'
+        )
+        self._log.info(
+            'Connecting to control_hat service'
+        )
+        while not self._control_client.wait_for_service(timeout_sec=5.0):
+            self._log.warn(
+                'control_hat service not available'
+            )
+        self._control_request.set_motors = 'ON'
+        self._log.info(
+                'Requesting motors ON async'
+        )
+        self._control_response = None
+        self._control_future = self._control_client.call_async(
+            self._control_request
+        )
+        self._log.info(
+            'Requested motors ON'
+        )
+
+        self._cmd_vel_timer = self.create_timer(
+            0.5,
+            self._move
+        )
+        self._log.info(
+            'cmd_vel timer started'
+        )
         self._log.info(
             'Navigator started'
+        )
+
+    def _move(self):
+        """Move the robot."""
+        if not self._control_future.done():
+            self._log.info(
+                'motors not yet ON'
+            )
+            return
+
+        self._cmd_vel_pub.publish(self._twist)
+        self._log.info(
+            '_move():'
+            f' {self._twist.twist.angular.z}'
         )
 
     def _detections_cb(self, msg):
         """Handle a received Detections message."""
         if msg.detections:
-            for detection in msg.detections:
-                self._log.debug(
-                    f'{detection.family}:{detection.id} ->'
-                    f' ({detection.centre.x:.0f},{detection.centre.y:.0f})'
-                )
+            x_diff = msg.detections[0].centre.x - self._camera['x_center']
+            if abs(x_diff) <= CLOSE_ENOUGH:
+                turn_toward = 'AHEAD'
+                self._twist.twist.angular.z = 0.0
+            elif x_diff < 0:
+                turn_toward = 'TURN_LEFT'
+                self._twist.twist.angular.z = 0.2
+            else:
+                turn_toward = 'TURN_RIGHT'
+                self._twist.twist.angular.z = -0.2
+
+            self._log.info(
+                f'{turn_toward}'
+            )
+        else:
+            turn_toward = 'LOST'
+            self._twist.twist.angular.z = 0.0
 
     def _tf_cb(self, msg):
         """Handle a received TF message."""
         if msg.transforms:
-            self._log.debug(
+            self._log.info(
                 'Received tf'
             )
 
@@ -67,6 +147,8 @@ def main(args=None):
 
     navigation = Navigation()
     rclpy.spin(navigation)
+    navigation._control_request.set_motors = 'ON'
+    navigation._control_client.call_async(navigation._control_request)
     rclpy.shutdown()
 
 
