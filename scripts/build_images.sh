e518310cdaf7e33c547706fe2cdf6e89c5eff46d
Bill Mania <bill@manialabs.us>
2026-07-14 10:37:36 -0500

    Added april_demo.py to the rq-lidar-april configuration. Updated april_demo.py and build_images.sh with locations of assorted files.
    https://github.com/billmania/roboquest_addons/issues/24

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,15 +2,20 @@
 
 cd /opt/projects/roboquest/ros2ws
 
-wget -O Dockerfile.roboquest_core https://github.com/billmania/roboquest/raw/refs/heads/main/ros2ws/Dockerfile.roboquest_core
-wget -O Dockerfile.roboquest_addons https://github.com/billmania/roboquest/raw/refs/heads/main/ros2ws/Dockerfile.roboquest_addons
+wget -O Dockerfile.roboquest_core \
+    https://github.com/billmania/roboquest/raw/refs/heads/main/ros2ws/Dockerfile.roboquest_core
+wget -O Dockerfile.roboquest_addons \
+    https://github.com/billmania/roboquest/raw/refs/heads/main/ros2ws/Dockerfile.roboquest_addons
 
 cd /opt/projects/roboquest/ros2ws/src
 rm -rf roboquest_core rq_msgs roboquest_addons
 
-git clone --branch 116-add-configuration-for-csi-arducam https://github.com/billmania/roboquest_core.git
-git clone https://github.com/billmania/rq_msgs.git
-git clone https://github.com/billmania/roboquest_addons.git
+git clone --branch 116-add-configuration-for-csi-arducam \
+    https://github.com/billmania/roboquest_core.git
+git clone \
+    https://github.com/billmania/rq_msgs.git
+git clone --branch 24-provide-a-shell-script-to-demonstrate-the-april-tag-and-lidar-functionality \
+    https://github.com/billmania/roboquest_addons.git
 
 cd /opt/projects/roboquest/ros2ws
 docker system prune -f
